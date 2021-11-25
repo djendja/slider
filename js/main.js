9a3e79616313aa2b4f20407bc9e2fe773ce8c537
Djendja <andjela.dj5@gmail.com>
2021-11-25 15:07:54 +0100

    first messy version of slider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-import {data} from "./data.js";
+import {
+    data
+} from "./data.js";
 class Slider {
     constructor(parent, data) {
         this.parent = parent;
@@ -7,28 +9,30 @@ class Slider {
 
     init() {
         this.selectors = {
-            ctx: ['slider'],
-            sliderContainer: ['slider__container', 'js-slider-container'],
-            sliderWrapper: ['slider__wrap', 'js-slider-wrap'],
-            img: ['slider__img', 'js-slider-img'],
-            button: ['slider__button','js-slider-button']
-        },
-        this.stateClasses = {
-            disabled: ['slider__button--disabled'],
-        },
-        this.left = {
-            classB: ['left'],
-            html: ['&#8617;']
+                ctx: ['slider'],
+                sliderContainer: ['slider__container', 'js-slider-container'],
+                sliderWrapper: ['slider__wrap', 'js-slider-wrap'],
+                img: ['slider__img', 'js-slider-img'],
+                button: ['slider__button', 'js-slider-button']
+            },
+            this.stateClasses = {
+                disabled: ['slider__button--disabled'],
+            },
+            this.left = {
+                classB: ['left'],
+                html: ['&#8617;']
 
-        },
-        this.right = {
-            classB: ['right'],
-            html: ['&#8618;']
+            },
+            this.right = {
+                classB: ['right'],
+                html: ['&#8618;']
 
-        }
-    
-        
+            }
+
+
         this.ctx = this.createCtx();
+        this.counter = 0;
+        
         this.createComponent();
 
     }
@@ -42,9 +46,9 @@ class Slider {
 
     createComponent() {
         let container = document.createElement("div");
-            container.classList.add(...this.selectors.sliderContainer);
+        container.classList.add(...this.selectors.sliderContainer);
         let wrapper = document.createElement("div");
-            wrapper.classList.add(...this.selectors.sliderWrapper);
+        wrapper.classList.add(...this.selectors.sliderWrapper);
         data.items.forEach(el => {
             let innerHtml = `<img class="slider__img js-img" src=${el.imgSrc}>`;
             wrapper.innerHTML += innerHtml;
@@ -65,7 +69,7 @@ class Slider {
         let button = document.createElement("button");
         button.classList.add(...this.selectors.button)
         button.classList.add(classB),
-        button.innerHTML = direction;
+            button.innerHTML = direction;
         return button;
     }
 
@@ -76,36 +80,38 @@ class Slider {
 
         let leftButton = this.ctx.querySelector('.left');
         let rightButton = this.ctx.querySelector('.right');
-        
+
         let counter = 0;
         let width = this.ctx.querySelector('.slider__wrap').offsetWidth;
-       
-        console.log(width);
+
+        // console.log(width);
         sliderWrap.style.transform = 'translateX(' + (-width * counter) + 'px)';
-        
+
 
 
         leftButton.addEventListener("click", () => {
-            console.log("left:"+counter);
-            if(counter === 0)  {
+            rightButton.classList.remove(this.stateClasses.disabled);
+            console.log("left:" + counter, imageCount);
+            if (counter === 0) {
                 leftButton.classList.add(this.stateClasses.disabled);
                 return;
-            }  
-             else {
-                leftButton.classList.remove(this.stateClasses.disabled); 
-                }
-                counter--;
-                sliderWrap.style.transform = 'translateX(' + (-width * counter) + 'px)';
-        });
-        rightButton.addEventListener("click",() => {
-            console.log("right"+counter);
-            if(counter == imageCount-1) { 
-               rightButton.classList.add(this.stateClasses.disabled)
-                return; 
+            } 
+            else {
+                leftButton.classList.remove(this.stateClasses.disabled);
             }
-           else {
-             rightButton.classList.remove(this.stateClasses.disabled); 
-        }
+            counter--;
+            sliderWrap.style.transform = 'translateX(' + (-width * counter) + 'px)';
+        });
+        rightButton.addEventListener("click", () => {
+            leftButton.classList.remove(this.stateClasses.disabled);
+            console.log("right" + counter, imageCount);
+            if (counter === imageCount - 1) {
+                rightButton.classList.add(this.stateClasses.disabled)
+                return;
+            } 
+            else {
+                rightButton.classList.remove(this.stateClasses.disabled);
+            }
             counter++;
             sliderWrap.style.transform = 'translateX(' + (-width * counter) + 'px)';
         });
@@ -121,7 +127,9 @@ function createSlider() {
 createSlider();
 createSlider();
 
-
+window.addEventListener("resize", () => {
+    console.log(window.innerWidth);
+})
 
 
 
@@ -132,11 +140,10 @@ createSlider();
 //         slider.style.transform = 'translateX(' + (-width * counter) + 'px)';
 //         
 //     }
-    
+
 //    slideToRight(slider, counter, width) {
 //        if(counter == imageCount -1) return;
 //         counter++;
 //         slider.style.transform = 'translateX(' + (-width * counter) + 'px)';
 //         
 //     }
-
